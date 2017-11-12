0deae31ca7f493e8dbe70d8c089a0d5c1956e6bc
SwordSongs <xiaoding_hupo@foxmail.com>
2017-11-12 23:48:36 +0800

    ShellSorting commit

---
+++ ShellSorting/ShellSorting/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void shellSorting(int a[],int len) {
+	int temp;
+	int step;
+	int i, j;
+	for (step = len / 2; step > 0;step /= 2) {
+		for (i = step; i < len; ++i) {        //找到步长元素
+			temp = a[i];
+			j = i - step;             //初始元素
+			if (a[j] > a[i]) {        //不满足顺序，交换
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	int array[] = {8, 5, 4, 9, 7, 6, 0, 1, 3, 2};
+	int len = sizeof(array) / sizeof(array[0]);
+
+	shellSorting(array,len);
+
+	for (auto i : array)
+		cout << i << "  ";
+
+	system("pause");
+	return 0;
+}
